84e34b097b8385fa2dd0318a22d64a0be0847ed3
cheese <goho302jo03@gmail.com>
2018-11-21 22:03:40 +0800

    Get stack maximum size

--- proj1/MergeSort/main.c
+++ proj1/MergeSort/main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/resource.h>
 
 #define list_entry(ptr, type, member) \
     ((type *) ((char *) (ptr) - (unsigned long) (&((type *) 0)->member)))
@@ -42,6 +43,10 @@ struct list_head {
 #define TRAVERSE_BW(HEAD, POS) \
     for (POS = ((HEAD)->prev); POS; \
             (POS = (POS->prev)), POS = (POS != ((HEAD)->prev) ? POS : NULL))
+
+int stack_size = 0;
+int max = 0;
+
 void init_list_head(struct list_head *hd) {
     hd->next = hd;
     hd->prev = hd;
@@ -86,12 +91,27 @@ static struct list_head *head_remove(struct list_head *list) {
     return new_head;
 }
 
+struct mylist {
+    int data;
+    char c;
+    struct list_head list;
+};
+
+
 static struct list_head *merge(struct list_head *left,
         struct list_head *right,
         bool (*pred)(struct list_head *,
             struct list_head *)) {
-    if (left == NULL) return right;
-    if (right == NULL) return left;
+    stack_size += (char *)__builtin_frame_address(1) - (char *)__builtin_frame_address(0);
+    max = max > stack_size ? max : stack_size;
+    if (left == NULL) {
+        stack_size -= (char *)__builtin_frame_address(1) - (char *)__builtin_frame_address(0);
+        return right;
+    }
+    if (right == NULL) {
+        stack_size -= (char *)__builtin_frame_address(1) - (char *)__builtin_frame_address(0);
+        return left;
+    }
     struct list_head *head, *tail;
     /* Fill your solution here */
     if(pred(left, right)) {
@@ -103,14 +123,10 @@ static struct list_head *merge(struct list_head *left,
     }
 
     tail = head_remove(right);
-    return head_add(head, merge(left, tail, pred));
+    struct list_head *tmp_head_add = head_add(head, merge(left, tail, pred));
+    stack_size -= (char *)__builtin_frame_address(1) - (char *)__builtin_frame_address(0);
+    return tmp_head_add;
 }
-
-struct mylist {
-    int data;
-    char c;
-    struct list_head list;
-};
 
 bool predicate(struct list_head *hdl, struct list_head *hdr) {
     struct mylist *l = list_entry(hdl, struct mylist, list);
@@ -119,13 +135,20 @@ bool predicate(struct list_head *hdl, struct list_head *hdr) {
 }
 
 struct list_head *merge_sort(struct list_head *head) {
+    stack_size += (char *)__builtin_frame_address(1) - (char *)__builtin_frame_address(0);
+    max = max > stack_size ? max : stack_size;
     if (head->next == head) {
+        stack_size -= (char *)__builtin_frame_address(1) - (char *)__builtin_frame_address(0);
         return head;
     }
     int c = COUNT(head);
     struct list_head *last_few = SLICE_BW(c / 2, head);
     struct list_head *first_few = SLICE(c % 2 ? c / 2 + 1 : c / 2, head);
-    return merge(merge_sort(first_few), merge_sort(last_few), predicate);
+    struct list_head *tmp;
+    tmp = merge(merge_sort(first_few), merge_sort(last_few), predicate);
+    stack_size -= (char *)__builtin_frame_address(1) - (char *)__builtin_frame_address(0);
+
+    return tmp;
 }
 
 void deinit(struct list_head *head) {
@@ -134,37 +157,44 @@ void deinit(struct list_head *head) {
 }
 
 int main() {
+
     struct mylist *temp;
     struct list_head *new_head;
+    int num = 1024;
     temp = malloc(sizeof *temp);
     init_list_head(&temp->list);
-    temp->data = 700;
+    temp->data = num;
     temp->c = 'a';
     new_head = &temp->list;
-    for (int i = 1; i < 13; i++) {
+
+    for (int i = 1; i < num; i++) {
         temp = malloc(sizeof *temp);
         init_list_head(&temp->list);
-        temp->data = rand() % 1000;
+        /* temp->data = rand() % 1000000; */
+        temp->data = num - i - 1;
         temp->c = 'a' + i;
         tail_add(&temp->list, new_head);
     }
-    struct list_head *curr;
-    curr = new_head;
-    do {
-        printf("%d(%c) ", list_entry(curr, struct mylist, list)->data, list_entry(curr, struct mylist, list)->c);
-        curr = curr->next;
-    } while(curr != new_head);
-    printf("\n*************\n");
+    /* struct list_head *curr; */
+    /* curr = new_head; */
+    /* do { */
+    /*     printf("%d ", list_entry(curr, struct mylist, list)->data); */
+    /*     curr = curr->next; */
+    /* } while(curr != new_head); */
+    /* printf("\n*************\n"); */
 
     new_head = merge_sort(new_head);
 
-    curr = new_head;
-    do {
-        printf("%d(%c) ", list_entry(curr, struct mylist, list)->data, list_entry(curr, struct mylist, list)->c);
-        curr = curr->next;
-    } while(curr != new_head);
-    printf("\n*************\n");
+    /* curr = new_head; */
+    /* do { */
+    /*     #<{(| printf("%d(%c) ", list_entry(curr, struct mylist, list)->data, list_entry(curr, struct mylist, list)->c); |)}># */
+    /*     printf("%d ", list_entry(curr, struct mylist, list)->data); */
+    /*     curr = curr->next; */
+    /* } while(curr != new_head); */
+    /* printf("\n*************\n"); */
 
+    printf("%d\n", stack_size);
+    printf("%d\n", max);
     clear(new_head, deinit);
     return 0;
 }
